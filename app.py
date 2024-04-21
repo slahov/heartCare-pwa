0be5374b059644a9ec2549282360d1a0dbf9a7ef
slahov <slavka.hovancova@student.tuke.sk>
2024-04-21 22:35:20 +0200

    updated predict function

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pk1', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 
 @app.route('/')
@@ -23,14 +24,38 @@ def serve_sw():
     return send_file('sw.js', mimetype='application/javascript')
 
 
+def convert_age(age):
+    if age <= 24:
+        return 1
+    elif age >= 80:
+        return 13
+    else:
+        return ((age - 24) // 5) + 2
+
+
 # registration of predict function
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
-    final = [np.array(int_features)]
+    diffWalk = float(request.form['DiffWalk'])
+    highBP = float(request.form['HighBP'])
+    highChol = float(request.form['HighChol'])
+    smoker = float(request.form['Smoker'])
+    stroke = float(request.form['Stroke'])
+    diabetes = float(request.form['Diabetes'])
+    genHealth = float(request.form['GenHlth'])
+    physHealth = float(request.form['PhysHlth'])
+    age = float(request.form['Age'])
+    income = float(request.form['Income'])
+
+    age_ordinal = convert_age(age)
+    features = np.array([
+        [diffWalk, highBP, highChol, smoker, stroke, diabetes, genHealth, physHealth, age_ordinal, income]
+    ])
+
+    final = scaler.transform(features)
     prediction = model.predict_proba(final)
-    output = '{0:.{1}f}'.format(prediction[0][1], 2)
-    return render_template('index.html', pred='Your risk of heart failure is {}'.format(output))
+    output = int(round(prediction[0][1] * 100))
+    return render_template('index.html', pred='Your risk of heart failure is {}%'.format(output), age_class=age_ordinal)
 
 
 if __name__ == '__main__':
