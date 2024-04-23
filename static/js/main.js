7339b81ef8b0f2a804a468579a6fe30089d0f943
slahov <slavka.hovancova@student.tuke.sk>
2024-04-23 23:40:00 +0200

    fixed scrolling to form animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,27 @@
-function scrollToForm()
-{
+function scrollToForm() {
     var targetElement = document.getElementById('row');
-    targetElement.scrollIntoView({ behaviour: 'smooth', block: 'start' });
+    var targetOffset = targetElement.getBoundingClientRect().top + window.pageYOffset;
+    var initialOffset = window.pageYOffset;
+    var distance = targetOffset - initialOffset;
+
+    var startTime = null;
+
+    function animateScroll(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+
+        var scrollProgress = Math.min(timeElapsed / 1000, 1);
+
+        var scrollPosition = initialOffset + distance * scrollProgress;
+
+        window.scrollTo(0, scrollPosition);
+
+        if (timeElapsed < 1000) {
+            requestAnimationFrame(animateScroll);
+        }
+    }
+
+    requestAnimationFrame(animateScroll);
 }
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
